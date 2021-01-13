5c2735d1506887a9fbd6e8ed8da1f35c45b1c852
kamil.mrowka <kamil.mrowka@outlook.com.au>
2021-01-13 14:55:42 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Login from './components/views/Login/Login';
-// import Tables from './components/views/Tables/Tables';
-// import Waiter from './components/views/Waiter/Waiter';
-// import Kitchen from './components/views/Kitchen/Kitchen';
-import { BrowserRouter} from 'react-router-dom';
+import Tables from './components/views/Tables/Tables';
+import Waiter from './components/views/Waiter/Waiter';
+import Kitchen from './components/views/Kitchen/Kitchen';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
       <MainLayout>
         <div className="App">
           <header className="App-header">
-            <Login text = 'hello'/>
             <p>
           Hello <code>src/App.js</code> and save to reload.
             </p>
@@ -27,10 +26,10 @@ function App() {
             </a>
           </header>
         </div>
-        {/* <Route exact path='/login' component={Login} /> */}
-        {/* <Route exact path='/tables' component={Tables} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/tables' component={Tables} />
         <Route exact path='/waiter' component={Waiter} />
-        <Route exact path='/kitchen' component={Kitchen} /> */}
+        <Route exact path='/kitchen' component={Kitchen} />
       </MainLayout>
     </BrowserRouter>
   );
