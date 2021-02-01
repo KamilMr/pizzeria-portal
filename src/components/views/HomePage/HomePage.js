c47dbc7082ec916cb6abae7d3eee88bb61591e9f
kamil.mrowka <kamil.mrowka@outlook.com.au>
2021-02-01 21:42:28 +0100

    add map to HomePage

--- src/components/views/HomePage/HomePage.js
+++ src/components/views/HomePage/HomePage.js
@@ -4,6 +4,14 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 
+const demoContent = [
+  {id: '1', type: 'event', time: '7 pm'},
+  {id: '2', type: 'event', time: '2 pm'},
+  {id: '3', type: 'online', time: '2 pm'},
+  {id: '4', type: 'online', time: '12 pm'},
+  {id: '5', type: 'local', time: '4 pm', table: 2},
+];
+
 const HomePage = () => (
   <div>
     <Toolbar />
@@ -20,22 +28,50 @@ const HomePage = () => (
       <Grid item xs={3}>
         <Paper className={styles.paper}>
           <h2>Todays Events</h2>
-          <p>1 event at 7 pm</p>
-          <p>1 event at 2 pm</p>
+          {demoContent.map(item => {
+            if(item.type === 'event'){
+              return (
+                <p>One event start at {item.time}</p>
+              );
+            }else {
+              return (
+                console.log('No more events')
+              );
+            }
+          })}
      
         </Paper>
       </Grid>
       <Grid item xs={3}>
         <Paper className={styles.paper}>
           <h2>Online Orders</h2>
-          <p>There are one order for 2 pm</p>
-          <p>There are one order for 12 pm</p>
+          {demoContent.map(item => {
+            if(item.type === 'online'){
+              return (
+                <p>There is one order to be delivered at {item.time}</p>
+              );
+            }else {
+              return (
+                console.log('No more orders')
+              );
+            }
+          })}
         </Paper>
       </Grid>
       <Grid item xs={3}>
         <Paper className={styles.paper}>
           <h2>Local Orders</h2>
-          <p>There are one table booked at 4 pm </p>
+          {demoContent.map(item => {
+            if(item.type === 'local'){
+              return (
+                <p>One reservation at {item.time} table: {item.table}</p>
+              );
+            }else {
+              return (
+                console.log('No more reservation')
+              );
+            }
+          })}
         </Paper>
       </Grid>
     </Grid>
